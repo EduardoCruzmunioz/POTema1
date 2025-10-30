12d1be58c72cd5af0b9189d8bfb8618721ba2413
Eduardo Cruz Muñoz <educruzm2004@gmail.com>
2025-10-30 14:42:44 +0100

    · Creacion de algunas constantes y variables

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
+     static void main(String[] args) {
+        final int PRECIO_HABITACION_INDIVIDUAL = 45;
+        final int PRECIO_HABITACION_DOBLE = 60;
+        final String NOMBRE_ADMIN= "C4rL0S";
+        final String ClAVE_ADMIN= "Gu4p0";
+        boolean salir = true;
         do {
-            System.out.println("""
-                    Menu 1
-                    ---
-                    
-                    
-                  
-                    """);
-        }while (true);
+
+        }while (salir);
     }
 }
